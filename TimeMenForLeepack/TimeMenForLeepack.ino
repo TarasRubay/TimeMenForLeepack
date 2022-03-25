e3f486745f62c7ab5c4332985a7c1536b10cd5c7
TarasRubay <84627325+TarasRubay@users.noreply.github.com>
2022-03-25 21:50:19 +0200

    ok

--- TimeMenForLeepack/TimeMenForLeepack.ino
+++ TimeMenForLeepack/TimeMenForLeepack.ino
@@ -1,50 +1,112 @@
-/*
- Name:		TimeMenForLeepack.ino
- Created:	2/5/2022 10:35:31 PM
- Author:	Rubay-PC
+/**
+   BasicHTTPSClient.ino
+
+    Created on: 20.08.2018
+
 */
 
-//#define BLYNK_PRINT Serial
+#include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFiMulti.h>
 
-char auth[] = "GQbktYuA_REdqQFtKji4-Df-VaEcuIR6";
+#include <ESP8266HTTPClient.h>
 
-char ssid[] = "DANA";
-char pass[] = "11160045";
+#include <WiFiClientSecureBearSSL.h>
+// Fingerprint for demo URL, expires on June 2, 2021, needs to be updated well before this date
+//const uint8_t fingerprint[20] = { 0x40, 0xaf, 0x00, 0x6b, 0xec, 0x90, 0x22, 0x41, 0x8e, 0xa3, 0xad, 0xfa, 0x1a, 0xe8, 0x25, 0x41, 0x1d, 0x1a, 0x54, 0xb3 };
+const char fingerprint[] = "4924e700d512b302b7e87be2c6ffb35d467f1acc";
 
-#define BLYNK_PRINT Serial
+ESP8266WiFiMulti WiFiMulti;
 
-WidgetTerminal terminal(V1);
-BLYNK_WRITE(V1)
-{
-    
-    
-}
+void setup() {
 
-void setup()
-{
-    // Debug console
     Serial.begin(9600);
+    // Serial.setDebugOutput(true);
 
-     Blynk.begin(auth, ssid, pass);
-   // Blynk.begin("eCIWL8JuGLRo9FMibxWmnEkKZn4iWWCeNG+utp8Qk6M=", ssid, pass, "ArduinoTimeMen.azure-devices.net");
-    terminal.clear();
+    Serial.println();
+    Serial.println();
+    Serial.println();
 
+    for (uint8_t t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(1000);
+    }
+
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP("DANA", "11160045");
 }
 
-void loop()
-{
-    Blynk.run();
-    String msg = Serial.readString();
-    if (msg.length() != 0) {
-    terminal.print(msg);
-    Serial.print(msg);
-    terminal.flush();
-    delay(1000);
-    terminal.print("NULL");
-    terminal.flush();
+void sendDataTest(String message) {
+    unsigned long time = millis();
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+        std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+
+        client->setFingerprint(fingerprint);
+        // Or, if you happy to ignore the SSL certificate, then use the following line instead:
+        // client->setInsecure();
+
+        HTTPClient https;
+
+        Serial.print("[HTTPS] begin...\n");
+        if (https.begin(*client, "https://pipeliner.azurewebsites.net/api/ESP8266")) {  // HTTPS
+
+            Serial.print("[HTTPS] POST...\n");
+
+            https.addHeader("Content-Type", "application/json");
+            https.addHeader("User-Agent", "Arduino");
+            https.addHeader("Accept", "*/*");
+            https.addHeader("Connection", "keep-alive");
+            int httpCode = https.POST(message);
+            // httpCode will be negative on error
+            if (httpCode > 0) {
+                // HTTP header has been send and Server response header has been handled
+                Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+
+                // file found at server
+                if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+                    String payload = https.getString();
+                    Serial.println(payload);
+                }
+            }
+            else {
+                Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
+            }
+            https.end();
+            //msg = "";
+        }
+        else {
+            Serial.printf("[HTTPS] Unable to connect\n");
+        }
+
+
     }
-    
+    Serial.println(millis() - time);
+}
+void sendData(String message) {
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+        std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+        client->setFingerprint(fingerprint);
+        HTTPClient https;
+        if (https.begin(*client, "https://pipeliner.azurewebsites.net/api/ESP8266")) {  // HTTPS
+            https.addHeader("Content-Type", "application/json");
+            https.addHeader("User-Agent", "Arduino");
+            https.addHeader("Accept", "*/*");
+            https.addHeader("Connection", "keep-alive");
+            String jsonText = "{ \"jsondata\": \"" + message + "\" }";
+            int httpCode = https.POST(jsonText);
+            https.end();
+        }
+    }
+}
+void loop() {
+
+    String msg = Serial.readString();
+    Serial.println(msg.length());
+    if (msg.length() != 0) {
+        sendDataTest(msg);
+        //sendData(msg);
+    }
 }
